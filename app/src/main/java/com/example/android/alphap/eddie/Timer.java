3081b9ba019dad7e52ab37c3b425f39f82ca9148
exmelenez <exmelendez@gmail.com>
2017-03-05 15:35:07 -0500

    Timer Class (java & XML), color changes + images

--- app/src/main/java/com/example/android/alphap/eddie/Timer.java
+++ app/src/main/java/com/example/android/alphap/eddie/Timer.java
@@ -1,7 +1,6 @@
 package com.example.android.alphap.eddie;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
@@ -21,7 +20,7 @@ public class Timer extends AppCompatActivity {
 
         progressBar=(ProgressBar)findViewById(R.id.progressBar);
 
-        myCountDownTimer = new MyCountDownTimer(20000, 1000);
+        myCountDownTimer = new MyCountDownTimer(5000, 1000);
         myCountDownTimer.start();
 
     }
@@ -36,11 +35,6 @@ public class Timer extends AppCompatActivity {
         public void onTick(long millisUntilFinished) {
 
             int progress = (int) (millisUntilFinished/1000);
-            if(progress <= 10 && progress > 5){
-                progressBar.setBackgroundColor(Color.BLUE);
-            }else if(progress <= 5){
-                progressBar.setBackgroundColor(Color.RED);
-            }
 
             progressBar.setProgress(progress);
         }
