a1ad34f89eef7276294925182039443384854311
RiditaNizam <riditan@gmail.com>
2017-03-07 19:51:59 -0500

    edits

--- app/src/main/java/com/example/android/alphap/Splash.java
+++ app/src/main/java/com/example/android/alphap/Splash.java
@@ -1,10 +1,14 @@
 package com.example.android.alphap;
 
+import android.animation.Animator;
 import android.animation.ObjectAnimator;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.animation.BounceInterpolator;
 import android.widget.ImageView;
+import com.example.android.alphap.gamemode.StartScreenActivity;
 
 public class Splash extends Activity {
 
@@ -20,12 +24,39 @@ public class Splash extends Activity {
         ObjectAnimator moveAnim = ObjectAnimator.ofFloat(potatoIV, "Y", 1000);
         moveAnim.setDuration(2000);
         moveAnim.setInterpolator(new BounceInterpolator());
+
+        moveAnim.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        Intent intent = new Intent(Splash.this, StartScreenActivity.class);
+                        startActivity(intent);
+
+                    }
+                }, 3000);
+
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
         moveAnim.start();
-
-
-
     }
-
-
 
 }
