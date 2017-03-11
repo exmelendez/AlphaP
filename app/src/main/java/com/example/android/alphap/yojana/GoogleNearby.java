431fb932ed683f6adb6248e95d02cd04d7228e44
SharmaYojana <joy.dfm@gmail.com>
2017-03-10 23:08:53 -0500

    google Nearby started

---
+++ app/src/main/java/com/example/android/alphap/yojana/GoogleNearby.java
@@ -0,0 +1,19 @@
+package com.example.android.alphap.yojana;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.android.alphap.R;
+
+/**
+ * Created by yojanasharma on 3/10/17.
+ */
+
+public class GoogleNearby extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_screen);
+    }
+}
