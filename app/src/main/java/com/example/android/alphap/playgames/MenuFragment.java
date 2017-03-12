1cf667b2730c6fcfafb9a17305a0484aef98a218
Ramona Harrison <ramonaliza@gmail.com>
2017-03-12 19:22:16 -0400

    Fix code style in GPG code

--- app/src/main/java/com/example/android/alphap/playgames/MenuFragment.java
+++ app/src/main/java/com/example/android/alphap/playgames/MenuFragment.java
@@ -11,56 +11,44 @@ import android.widget.Button;
 
 import com.example.android.alphap.R;
 
-public class MenuFragment extends Fragment
-{
+public class MenuFragment extends Fragment {
     private Listener listener;
 
     @Override
-    public void onAttach(Context context)
-    {
+    public void onAttach(Context context) {
         super.onAttach(context);
-        if(context instanceof Listener)
-        {
+        if (context instanceof Listener) {
             listener = (Listener) context;
-        }
-        else
-        {
+        } else {
             throw new RuntimeException(context.toString() + " must implement Listener");
         }
     }
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
-    {
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View v = inflater.inflate(R.layout.fragment_menu, container, false);
 
         Button createGameButton = (Button) v.findViewById(R.id.button_create_game);
-        createGameButton.setOnClickListener(new View.OnClickListener()
-        {
+        createGameButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 listener.onCreateGameClicked();
             }
         });
 
         Button joinGameButton = (Button) v.findViewById(R.id.button_join_game);
-        joinGameButton.setOnClickListener(new View.OnClickListener()
-        {
+        joinGameButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 listener.onJoinGameClicked();
             }
         });
 
         Button signOutButton = (Button) v.findViewById(R.id.button_sign_out);
-        signOutButton.setOnClickListener(new View.OnClickListener()
-        {
+        signOutButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 listener.onSignOutClicked();
             }
         });
@@ -68,8 +56,7 @@ public class MenuFragment extends Fragment
         return v;
     }
 
-    interface Listener
-    {
+    interface Listener {
         void onCreateGameClicked();
 
         void onJoinGameClicked();
