5c4f4b29c1fc5fcdd8e2cda8dc468e64a8787cd3
Asia <gibsonjasia@gmail.com>
2017-03-12 17:28:35 -0400

    add shake visible/invisible

--- app/src/main/java/com/example/android/alphap/shakefunction/ShakeActivity.java
+++ app/src/main/java/com/example/android/alphap/shakefunction/ShakeActivity.java
@@ -5,6 +5,8 @@ import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,6 +22,7 @@ public class ShakeActivity extends AppCompatActivity {
     private ShakeDetect mShakeDetector;
 
     TextView tv;
+    ImageView potatoDude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +30,8 @@ public class ShakeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         tv = (TextView) findViewById(R.id.test);
+        potatoDude = (ImageView) findViewById(R.id.game_potato);
+
 
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mAccelerometer = mSensorManager
@@ -47,14 +52,25 @@ public class ShakeActivity extends AppCompatActivity {
     private void handleShakeEvent(int count) {
         switch (count) {
             case 1:
+                potatoDude.setVisibility(View.INVISIBLE);
                 Toast.makeText(tv.getContext(), "1 SHAKE", Toast.LENGTH_SHORT).show();
                 break;
             case 2:
+                potatoDude.setVisibility(View.VISIBLE);
                 Toast.makeText(tv.getContext(), "2ND SHAKE", Toast.LENGTH_SHORT).show();
                 break;
             case 3:
+                potatoDude.setVisibility(View.INVISIBLE);
                 Toast.makeText(tv.getContext(), "3RD SHAAKE", Toast.LENGTH_SHORT).show();
                 break;
+            case 4:
+                potatoDude.setVisibility(View.VISIBLE);
+                Toast.makeText(potatoDude.getContext(), "YOUR TATER POPPED", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                potatoDude.setVisibility(View.VISIBLE);
+                break;
+
         }
     }
 
