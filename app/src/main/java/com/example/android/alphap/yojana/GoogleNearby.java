726278094f89f2681b920d4cb5064dbeb9a84888
SharmaYojana <joy.dfm@gmail.com>
2017-03-13 18:12:01 -0400

    Google Nearby

--- app/src/main/java/com/example/android/alphap/yojana/GoogleNearby.java
+++ app/src/main/java/com/example/android/alphap/yojana/GoogleNearby.java
@@ -1,19 +1,114 @@
 package com.example.android.alphap.yojana;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.example.android.alphap.R;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.games.Games;
+import com.google.android.gms.games.multiplayer.Multiplayer;
+import com.google.android.gms.games.multiplayer.realtime.RealTimeMessageReceivedListener;
+import com.google.android.gms.games.multiplayer.realtime.RoomConfig;
+import com.google.android.gms.games.multiplayer.realtime.RoomStatusUpdateListener;
+import com.google.android.gms.games.multiplayer.realtime.RoomUpdateListener;
+
+import java.util.ArrayList;
 
 /**
  * Created by yojanasharma on 3/10/17.
  */
 
-public class GoogleNearby extends Activity {
+public class GoogleNearby extends Activity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start_screen);
+        setContentView(R.layout.google_nearby);
     }
-}
+
+    private GoogleApiClient mGoogleApiClient;
+    private static final long ROLE_FARMER = 0x1; // 001 in binary
+    private static final long ROLE_ARCHER = 0x2; // 010 in binary
+    private static final long ROLE_WIZARD = 0x4;  // 100 in binary
+    final static int RC_SELECT_PLAYERS = 10000;
+
+
+    private void GoogleNearby(long role) {
+        // auto-match criteria to invite one random automatch opponent.
+        // You can also specify more opponents (up to 3).
+        Bundle am = RoomConfig.createAutoMatchCriteria(2, 2, role);
+
+        // build the room config:
+        RoomConfig.Builder roomConfigBuilder = makeBasicRoomConfigBuilder();
+        roomConfigBuilder.setAutoMatchCriteria(am);
+        RoomConfig roomConfig = roomConfigBuilder.build();
+
+        // create room:
+        Games.RealTimeMultiplayer.create(mGoogleApiClient, roomConfig);
+
+        // prevent screen from sleeping during handshake
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        // go to game screen
+
+
+// launch the player selection screen
+// minimum: 1 other player; maximum: 3 other players
+        Intent intent = Games.RealTimeMultiplayer.getSelectOpponentsIntent(mGoogleApiClient, 1, 3);
+        startActivityForResult(intent, RC_SELECT_PLAYERS);
+    }
+
+    @Override
+    public void onActivityResult(int request, int response, Intent data) {
+        if (request == RC_SELECT_PLAYERS) {
+            if (response != Activity.RESULT_OK) {
+                // user canceled
+                return;
+            }
+
+            // get the invitee list
+            Bundle extras = data.getExtras();
+            final ArrayList<String> invitees =
+                    data.getStringArrayListExtra(Games.EXTRA_PLAYER_IDS);
+
+            // get auto-match criteria
+            Bundle autoMatchCriteria = null;
+            int minAutoMatchPlayers =
+                    data.getIntExtra(Multiplayer.EXTRA_MIN_AUTOMATCH_PLAYERS, 0);
+            int maxAutoMatchPlayers =
+                    data.getIntExtra(Multiplayer.EXTRA_MAX_AUTOMATCH_PLAYERS, 0);
+
+            if (minAutoMatchPlayers > 0) {
+                autoMatchCriteria = RoomConfig.createAutoMatchCriteria(
+                        minAutoMatchPlayers, maxAutoMatchPlayers, 0);
+            } else {
+                autoMatchCriteria = null;
+            }
+
+            // create the room and specify a variant if appropriate
+            RoomConfig.Builder roomConfigBuilder = makeBasicRoomConfigBuilder();
+            roomConfigBuilder.addPlayersToInvite(invitees);
+            if (autoMatchCriteria != null) {
+                roomConfigBuilder.setAutoMatchCriteria(autoMatchCriteria);
+            }
+            RoomConfig roomConfig = roomConfigBuilder.build();
+            Games.RealTimeMultiplayer.create(mGoogleApiClient, roomConfig);
+
+            // prevent screen from sleeping during handshake
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        }
+    }
+
+    // create a RoomConfigBuilder that's appropriate for your implementation
+    private RoomConfig.Builder makeBasicRoomConfigBuilder() {
+        return RoomConfig.builder((RoomUpdateListener) this)
+                .setMessageReceivedListener((RealTimeMessageReceivedListener) this)
+                .setRoomStatusUpdateListener((RoomStatusUpdateListener) this);
+    }
+
+
+
+    }
+
