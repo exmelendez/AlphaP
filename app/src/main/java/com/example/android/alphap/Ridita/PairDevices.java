8b6812a3709c784bcf3d7be46b0278454e62b120
RiditaNizam <riditan@gmail.com>
2017-03-13 20:09:46 -0400

    unknown

---
+++ app/src/main/java/com/example/android/alphap/Ridita/PairDevices.java
@@ -0,0 +1,15 @@
+package com.example.android.alphap.Ridita;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.android.alphap.R;
+
+public class PairDevices extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pair_devices);
+    }
+}
