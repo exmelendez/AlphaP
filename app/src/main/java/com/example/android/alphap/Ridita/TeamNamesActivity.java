0a435bfaf9c17619feb0472bc55d98fa91ee0e53
RiditaNizam <riditan@gmail.com>
2017-03-15 18:17:49 -0400

    XML for Creators

--- app/src/main/java/com/example/android/alphap/Ridita/TeamNamesActivity.java
+++ app/src/main/java/com/example/android/alphap/Ridita/TeamNamesActivity.java
@@ -3,13 +3,13 @@ package com.example.android.alphap.Ridita;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.example.android.alphap.R;
+import com.ramonaharrison.gametest1.R;
 
-public class PairDevices extends AppCompatActivity {
+public class TeamNamesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pair_devices);
+        setContentView(R.layout.activity_team_names);
     }
 }
